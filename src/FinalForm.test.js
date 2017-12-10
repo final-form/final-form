4a5f0fc44962276ab66a797eb1403ccb8894b5eb
Erik Rasmussen <rasmussenerik@gmail.com>
2017-12-10 20:22:21 +0100

    Added check that a field is registered before setting error

--- src/FinalForm.test.js
+++
@@ -1,9 +0,0 @@
-describe('FinalForm', () => {
-  require('./FinalForm.batching.test')
-  require('./FinalForm.creation.test')
-  require('./FinalForm.debugging.test')
-  require('./FinalForm.fieldSubscribing.test')
-  require('./FinalForm.submission.test')
-  require('./FinalForm.subscribing.test')
-  require('./FinalForm.validating.test')
-})
