8890b74cae877019a4a3df754500d15e6421152c
Erik Rasmussen <rasmussenerik@gmail.com>
2017-12-13 11:30:07 +0100

    Updated version string

--- src/FinalForm.js
+++ src/FinalForm.js
@@ -26,7 +26,7 @@ import type {
 } from './types'
 
 export const FORM_ERROR = Symbol('form-error')
-export const version = '1.3.0'
+export const version = '1.3.3'
 
 type Subscribers<T: Object> = {
   index: number,
