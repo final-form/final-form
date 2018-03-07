7a64968ca2f5d5d2827ba989fb23e834b1e32818
Forbes Lindesay <forbes@lindesay.co.uk>
2018-03-07 15:06:38 +0000

    Add method for setting config value after form creation (#87)
    * Add method for setting config value after form creation
    
    [see final-form/react-final-form#128]
    
    * Add test suite for setConfig and re-run validation on replacing the `validate` fn
    
    * Undo prettier formatting of AnyObject
    
    * add if(mutators) check
    
    * Remove unused variables

--- src/FinalForm.js
+++ src/FinalForm.js
@@ -121,7 +121,7 @@ const createForm = (config: Config): FormApi => {
   if (!config) {
     throw new Error('No config specified')
   }
-  const {
+  let {
     debug,
     initialValues,
     mutators,
@@ -171,28 +171,32 @@ const createForm = (config: Config): FormApi => {
   }
 
   // bind state to mutators
+  const getMutatorApi = key => (...args) => {
+    if (mutators) {
+      const mutatableState = {
+        formState: state.formState,
+        fields: state.fields
+      }
+      const returnValue = mutators[key](args, mutatableState, {
+        changeValue,
+        getIn,
+        setIn,
+        shallowEqual
+      })
+      state.formState = mutatableState.formState
+      state.fields = mutatableState.fields
+      runValidation(undefined, () => {
+        notifyFieldListeners()
+        notifyFormListeners()
+      })
+      return returnValue
+    }
+  }
+  
   const mutatorsApi =
     (mutators &&
       Object.keys(mutators).reduce((result, key) => {
-        result[key] = (...args) => {
-          const mutatableState = {
-            formState: state.formState,
-            fields: state.fields
-          }
-          const returnValue = mutators[key](args, mutatableState, {
-            changeValue,
-            getIn,
-            setIn,
-            shallowEqual
-          })
-          state.formState = mutatableState.formState
-          state.fields = mutatableState.fields
-          runValidation(undefined, () => {
-            notifyFieldListeners()
-            notifyFormListeners()
-          })
-          return returnValue
-        }
+        result[key] = getMutatorApi(key)
         return result
       }, {})) ||
     {}
@@ -629,6 +633,49 @@ const createForm = (config: Config): FormApi => {
         })
       }
       validationBlocked = false
+    },
+
+    setConfig: (name: string, value: any) => {
+      switch (name) {
+        case 'debug':
+          debug = value
+          break
+        case 'initialValues':
+          api.initialize(value)
+          break
+        case 'mutators':
+          mutators = value
+          if (value) {
+            Object.keys(mutatorsApi).forEach(key => {
+              if (!(key in value)) {
+                delete mutatorsApi[key]
+              }
+            })
+            Object.keys(value).forEach(key => {
+              mutatorsApi[key] = getMutatorApi(key)
+            })
+          } else {
+            Object.keys(mutatorsApi).forEach(key => {
+              delete mutatorsApi[key]
+            })
+          }
+          break
+        case 'onSubmit':
+          onSubmit = value
+          break
+        case 'validate':
+          validate = value
+          runValidation(undefined, () => {
+            notifyFieldListeners()
+            notifyFormListeners()
+          })
+          break
+        case 'validateOnBlur':
+          validateOnBlur = value
+          break
+        default:
+          throw new Error('Unrecognised option ' + name)
+      }
     },
 
     submit: () => {
---
+++ src/FinalForm.setConfig.test.js
@@ -0,0 +1,214 @@
+import createForm from './FinalForm'
+
+const onSubmitMock = (values, callback) => {}
+
+describe('FinalForm.setConfig', () => {
+  it('should update debug callback on setConfig("debug", fn)', () => {
+    const debug = jest.fn()
+    const form = createForm({ onSubmit: onSubmitMock })
+
+    form.registerField('foo', () => {})
+    expect(debug).toHaveBeenCalledTimes(0)
+
+    form.change('foo', 'bar')
+
+    expect(debug).toHaveBeenCalledTimes(0)
+
+    form.setConfig('debug', debug)
+
+    form.change('foo', 'bing')
+
+    expect(debug).toHaveBeenCalledTimes(1)
+  })
+
+  it('should initialize the form on setConfig("initialValues", values)', () => {
+    const form = createForm({
+      onSubmit: onSubmitMock,
+      initialValues: {
+        foo: 'bar'
+      }
+    })
+    const spy = jest.fn()
+    form.registerField('foo', spy, { initial: true })
+
+    // should initialize with initial value
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy.mock.calls[0][0].initial).toBe('bar')
+
+    form.reset()
+
+    // same initial value, duh
+    expect(spy).toHaveBeenCalledTimes(1)
+
+    form.setConfig('initialValues', { foo: 'baz' })
+
+    // new initial value
+    expect(spy).toHaveBeenCalledTimes(2)
+    expect(spy.mock.calls[1][0].initial).toBe('baz')
+  })
+
+  it('should update mutators on setConfig("mutators", mutators)', () => {
+    const clear = jest.fn(([name], state, { changeValue }) => {
+      changeValue(state, name, () => undefined)
+    })
+    const upper = jest.fn(([name], state, { changeValue }) => {
+      changeValue(state, name, value => value && value.toUpperCase())
+    })
+
+    const form = createForm({
+      onSubmit: onSubmitMock,
+      mutators: { clear }
+    })
+    expect(form.mutators).toBeDefined()
+    expect(form.mutators.clear).toBeDefined()
+    expect(typeof form.mutators.clear).toBe('function')
+    expect(typeof form.mutators.upper).toBe('undefined')
+
+    form.setConfig('mutators', { clear, upper })
+
+    expect(form.mutators.upper).toBeDefined()
+    expect(typeof form.mutators.upper).toBe('function')
+
+    const formListener = jest.fn()
+    form.subscribe(formListener, { values: true })
+    form.registerField('foo', () => {}, { value: true })
+
+    expect(formListener).toHaveBeenCalledTimes(1)
+    expect(formListener.mock.calls[0][0].values).toEqual({})
+
+    form.change('foo', 'bar')
+
+    expect(formListener).toHaveBeenCalledTimes(2)
+    expect(formListener.mock.calls[1][0].values.foo).toBe('bar')
+
+    form.mutators.upper('foo')
+
+    expect(formListener).toHaveBeenCalledTimes(3)
+    expect(formListener.mock.calls[2][0].values.foo).toBe('BAR')
+
+    form.mutators.clear('foo')
+
+    expect(formListener).toHaveBeenCalledTimes(4)
+    expect(formListener.mock.calls[3][0].values.foo).toBeUndefined()
+
+    form.setConfig('mutators', { upper })
+    expect(typeof form.mutators.clear).toBe('undefined')
+
+    form.setConfig('mutators', undefined)
+    expect(typeof form.mutators.clear).toBe('undefined')
+    expect(typeof form.mutators.upper).toBe('undefined')
+  })
+
+  it('should replace onSubmit on setConfig("onSubmit", fn)', () => {
+    const onSubmit = jest.fn()
+    const form = createForm({ onSubmit })
+    form.registerField('foo', () => {})
+    form.registerField('foo2', () => {})
+
+    form.change('foo', 'bar')
+    form.change('foo2', 'baz')
+
+    const onSubmitReplacement = jest.fn()
+    form.setConfig('onSubmit', onSubmitReplacement)
+    expect(onSubmit).not.toHaveBeenCalled()
+    expect(onSubmitReplacement).not.toHaveBeenCalled()
+    form.submit()
+    expect(onSubmit).not.toHaveBeenCalled()
+    expect(onSubmitReplacement.mock.calls[0][0]).toEqual({
+      foo: 'bar',
+      foo2: 'baz'
+    })
+  })
+
+  it('should update validate on setConfig("validate", fn)', () => {
+    const onSubmit = jest.fn()
+    const form = createForm({
+      onSubmit,
+      validate: values => {
+        const errors = {}
+        if (!values.username) {
+          errors.username = 'Required'
+        }
+        return errors
+      }
+    })
+    const username = jest.fn()
+    form.registerField('username', username, { error: true })
+    expect(username).toHaveBeenCalledTimes(1)
+    expect(username.mock.calls[0][0].error).toBe('Required')
+
+    expect(onSubmit).not.toHaveBeenCalled()
+    form.submit()
+    expect(onSubmit).not.toHaveBeenCalled()
+
+    form.setConfig('validate', () => ({}))
+
+    // username is no longer required
+    expect(username).toHaveBeenCalledTimes(2)
+    expect(username.mock.calls[1][0].error).toBe(undefined)
+
+    // form is valid now, so submit should work
+    expect(onSubmit).not.toHaveBeenCalled()
+    form.submit()
+    expect(onSubmit).toHaveBeenCalledTimes(1)
+    expect(onSubmit.mock.calls[0][0].username).toBe(undefined)
+  })
+
+  it('should replace validateOnBlur on setConfig("validateOnBlur", value)', () => {
+    const validate = jest.fn(values => {
+      const errors = {}
+      if (!values.foo) {
+        errors.foo = 'Required'
+      }
+      return errors
+    })
+    const form = createForm({
+      onSubmit: onSubmitMock,
+      validate,
+      validateOnBlur: false
+    })
+
+    expect(validate).toHaveBeenCalledTimes(1)
+
+    const spy = jest.fn()
+    form.registerField('foo', spy, { error: true })
+
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy.mock.calls[0][0].error).toBe('Required')
+    expect(validate).toHaveBeenCalledTimes(2)
+
+    form.setConfig('validateOnBlur', true)
+
+    form.focus('foo')
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(validate).toHaveBeenCalledTimes(2) // not called on focus
+    form.change('foo', 'typing')
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(validate).toHaveBeenCalledTimes(2)
+    form.blur('foo')
+    expect(spy).toHaveBeenCalledTimes(2)
+    expect(spy.mock.calls[1][0].error).toBeUndefined()
+    expect(validate).toHaveBeenCalledTimes(3) // called on blur
+
+    form.setConfig('validateOnBlur', false)
+
+    form.focus('foo')
+    expect(spy).toHaveBeenCalledTimes(2)
+    expect(validate).toHaveBeenCalledTimes(3) // not called on focus
+    form.change('foo', 'typing something else')
+    expect(spy).toHaveBeenCalledTimes(2)
+    expect(validate).toHaveBeenCalledTimes(4) // called on change because we set validateOnBlur=false
+    form.blur('foo')
+    expect(spy).toHaveBeenCalledTimes(2)
+    expect(validate).toHaveBeenCalledTimes(4) // not called on blur
+  })
+
+  it('should throw on unknown names', () => {
+    const form = createForm({
+      onSubmit: onSubmitMock
+    })
+    expect(() => {
+      form.setConfig('whatever', false)
+    }).toThrowError('Unrecognised option whatever')
+  })
+})
