998989527218c3b09077b45d6db3f29a313bb5ee
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-04-11 14:02:11 +0200

    Prefer simple for loop over for-of (it doesnt have to be transpiled)

--- src/structure/getIn.js
+++ src/structure/getIn.js
@@ -6,7 +6,8 @@ const getIn: GetIn = (state: Object, complexKey: string): any => {
   // Intentionally using iteration rather than recursion
   const path = toPath(complexKey)
   let current: any = state
-  for (let key of path) {
+  for (let i = 0; i < path.length; i++) {
+    const key = path[i]
     if (
       current === undefined ||
       current === null ||
