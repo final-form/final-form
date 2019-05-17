456cc68e7c2ee43d24a9aeac57bc15abbc060c63
Ilya Ermolaev <i.a.ermolaev@tinkoff.ru>
2019-05-17 15:31:39 +0300

    Change method split on match

--- src/structure/toPath.js
+++ src/structure/toPath.js
@@ -6,7 +6,7 @@ const toPath = (key: string): string[] => {
   if (typeof key !== 'string') {
     throw new Error('toPath() expects a string')
   }
-  return key.split(/[.[\]]+/).filter(Boolean)
+  return key.match(/[^[\].]+/g) || []
 }
 
 export default toPath
