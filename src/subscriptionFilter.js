807ddaea2fdb2420c9afc124147e13cd3956de1a
Cheng <liucheng.tech@outlook.com>
2019-08-08 21:53:55 +0800

    :sparkles: subscription shape

--- src/subscriptionFilter.js
+++ src/subscriptionFilter.js
@@ -12,14 +12,39 @@ export default function<T: { [string]: any }>(
   keys.forEach(key => {
     if (subscription[key]) {
       dest[key] = src[key]
-      if (
-        !previous ||
-        (~shallowEqualKeys.indexOf(key)
-          ? !shallowEqual(src[key], previous[key])
-          : src[key] !== previous[key])
+      if (!previous) {
+        different = true
+      } else if (
+        ~shallowEqualKeys.indexOf(key) &&
+        !shallowEqual(src[key], previous[key])
       ) {
         different = true
+      } else if (
+        ~['values', 'value'].indexOf(key) &&
+        'object' === typeof subscription[key]
+      ) {
+        different = checkDiffByShape(src[key], previous[key], subscription[key])
+      } else if (src[key] !== previous[key]) {
+        different = true
       }
+    }
+  })
+  return different
+}
+
+function checkDiffByShape(a, b, shape) {
+  if (!a || !b) {
+    return a !== b
+  }
+  let different = false
+  Object.keys(shape).forEach(k => {
+    if (different) {
+      return
+    }
+    if ('object' === typeof shape[k]) {
+      different = different || checkDiffByShape(a[k], b[k], shape[k])
+    } else {
+      different = different || !shallowEqual(a[k], b[k])
     }
   })
   return different
